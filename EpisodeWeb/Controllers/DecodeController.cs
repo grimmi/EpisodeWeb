33933799cb238f16f8da344b5158ee41b4f9039d
Arne Lehmann <arne.lehmann@gmx.de>
2017-05-30 20:04:28 +0200

    initial frame for web interface

---
+++ EpisodeWeb/Controllers/DecodeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EpisodeWeb.Controllers
+{
+    [Route("api/[controller]")]
+    public class DecodeController : Controller
+    {
+        [HttpGet]
+        public JObject EncodedFiles()
+        {
+            var files = Directory.GetFiles(@"z:\downloads", "*.otrkey")
+                .OrderBy(f => Path.GetFileName(f))
+                .Select((f, i) => new { Path = Path.GetFileName(f), Index = i });
+
+            var response = new JObject();
+            response.Add("files", JToken.FromObject(files));
+
+            return response;
+        }
+    }
+}
