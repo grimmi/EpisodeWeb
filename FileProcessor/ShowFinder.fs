e17f928d6a9bfe1201f64e3b74c5ab891c2216cd
Arne Lehmann <arne.lehmann@gmx.de>
2017-06-04 10:55:11 +0200

    reworked the interface to tvdb.com and the showfinder

---
+++ FileProcessor/ShowFinder.fs
@@ -0,0 +1,35 @@
+﻿module ShowFinder
+
+open Cache
+open Newtonsoft.Json
+open System.Net
+open Types
+open TvApi
+
+type ShowFinder(c: Cache, a: TvApi) =
+    let cache = c
+    let api = a
+
+    let searchShow show = async {
+        let! showResponse = api.GetAsync("/search/series?name=" + show)
+        return JsonConvert.DeserializeObject<SearchResponse>(showResponse) }
+
+    member this.FindShow showName = 
+        let rec findShowInDb original current = async{
+            try
+                match cache.TryGetShow current with
+                |true, mappedShow -> return Some(mappedShow)
+                |false, _ ->    let! shows = searchShow current
+                                let chosenShow = match shows.data with
+                                                 |s when s |> Seq.length > 0 -> Some(s |> Seq.head)
+                                                 |_ -> None
+                                match chosenShow with
+                                |Some show -> cache.CacheShow original show      
+                                              return Some(show)
+                                |None -> return None
+            with
+                | :? WebException as ex ->  return None
+        }
+        match showName with
+        |None -> async { return  None }
+        |Some n -> findShowInDb n n
---
+++ FileProcessor/TvApi.fs
@@ -0,0 +1,59 @@
+﻿module TvApi
+
+open Newtonsoft.Json.Linq
+open System
+open System.IO
+open System.Net
+open System.Text
+open Newtonsoft.Json
+open Types
+
+type TvApi() =
+    let mutable apikey = ""
+    let mutable user = ""
+    let mutable userkey = ""
+    let mutable token = ""
+
+
+    let extractValue (line:string) = 
+        (line.Split '=').[1].Trim()
+
+    let readcredentials = 
+        match File.ReadAllLines("./credentials.cred") |> Array.take 3 with
+        |[|keyline;userline;pwline|] ->
+            apikey <- keyline |> extractValue
+            user <- userline |> extractValue
+            userkey <- pwline |> extractValue
+        |_ -> raise (Exception "invalid configuration")
+
+    do readcredentials
+
+    let apiUrl = "https://api.thetvdb.com"
+
+    let login = 
+        let t = async {
+            use client = new WebClient()
+            let body = "{\"apikey\":\"" + apikey + "\", \"username\":\"" + user + "\", \"userkey\":\"" + userkey + "\"}" 
+            client.Headers.Set("Content-Type", "application/json")
+            let! response = client.UploadDataTaskAsync(apiUrl + "/login", "POST",  body |> Encoding.UTF8.GetBytes) |> Async.AwaitTask
+            let jsonToken = JObject.Parse(Encoding.UTF8.GetString response)
+            return jsonToken.["token"] |> string } |> Async.RunSynchronously
+        token <- t
+
+    let rec getAuthorizedClient t =
+        match t with
+        |"" -> login
+               getAuthorizedClient token
+        |_ -> let client = new WebClient()
+              client.Encoding <- Encoding.UTF8
+              client.Headers.Set("Content-Type", "application/json")
+              client.Headers.Set("Authorization", "Bearer " + t)
+              client.Headers.Set("Accept-Language", "en")
+              client
+
+    member this.GetAsync uri = async {
+        let client = getAuthorizedClient token
+        let! response = client.AsyncDownloadString(Uri(apiUrl + uri))
+        return response
+    }
+    
