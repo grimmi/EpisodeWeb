29ca8511ebe02b76c4074718fe4925e4c02192e6
Arne Lehmann <arne.lehmann@gmx.de>
2017-06-04 10:34:29 +0200

    completed Parser

--- FileProcessor/Parser.fs
+++ FileProcessor/Parser.fs
@@ -1,11 +1,11 @@
-﻿namespace FileProcessor
+﻿module Parser
 
+open Cache
 open System
 open System.IO
 
-type Processor(f: string, c: Cache) = 
+type Parser(f: string) = 
     let file = f
-    let cache = c
 
     member this.ParseShowName =
         let filename = Path.GetFileName file
@@ -17,6 +17,25 @@ type Processor(f: string, c: Cache) =
             let pointIdx = filename.IndexOf '.'
             Some(filename.Substring(0, pointIdx - 3).Replace("_", " "))
 
-    member this.FindShow =
-        
+    member this.ParseEpisodeName (file:string) = 
+        let filename = Path.GetFileName file
+        let pointIdx = filename.IndexOf '.'
+        let dblUnderscoreIdx = filename.IndexOf "__"
+
+        if pointIdx = -1 || dblUnderscoreIdx = -1 then
+            None
+        else
+            let length = (pointIdx - 3) - (dblUnderscoreIdx + 2)
+            match length with
+            |_ when length > 0 -> Some(filename.Substring(dblUnderscoreIdx + 2, length).Replace("_", " "))
+            |_ -> None
+
+    member this.ParseDate (file:string) = 
+        let filename = Path.GetFileName file
+        let pointIdx = filename.IndexOf '.'
+        match pointIdx with
+        |(-1) -> None
+        |_ -> 
+            let datePart = filename.Substring(pointIdx - 2, 8)
+            Some(DateTime.ParseExact(datePart, "yy.MM.dd", null))
         
