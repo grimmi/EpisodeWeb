be69e870d2256910065b0f0cf3375ae4ee50a603
Arne Lehmann <arne.lehmann@gmx.de>
2017-06-05 14:04:55 +0200

    reworked findshow

--- FileProcessor/ShowFinder.fs
+++ FileProcessor/ShowFinder.fs
@@ -11,25 +11,26 @@ type ShowFinder(c: Cache, a: TvApi) =
     let api = a
 
     let searchShow show = async {
-        let! showResponse = api.GetAsync("/search/series?name=" + show)
-        return JsonConvert.DeserializeObject<SearchResponse>(showResponse) }
-
-    member this.FindShow showName = 
-        let rec findShowInDb original current = async{
-            try
-                match cache.TryGetShow current with
-                |true, mappedShow -> return Some(mappedShow)
-                |false, _ ->    let! shows = searchShow current
-                                let chosenShow = match shows.data with
-                                                 |s when s |> Seq.length > 0 -> Some(s |> Seq.head)
-                                                 |_ -> None
-                                match chosenShow with
-                                |Some show -> cache.CacheShow original show      
-                                              return Some(show)
-                                |None -> return None
-            with
-                | :? WebException as ex ->  return None
+        try
+            let! showResponse = api.GetAsync("/search/series?name=" + show)
+            return Some(JsonConvert.DeserializeObject<SearchResponse>(showResponse))
+        with
+            | :? WebException as ex -> return None
         }
-        match showName with
-        |None -> async { return  None }
-        |Some n -> findShowInDb n n
+
+    member this.FindShow showName = async {
+        let (found, show) = cache.TryGetShow showName
+        match (found, show) with
+        |(true, _) -> return Some show
+        |(false, _) -> 
+            let! dbResult = searchShow showName
+            match dbResult with
+            |Some response ->   let dbShows = response.data |> List.ofSeq
+                                match dbShows with
+                                |[] -> return None
+                                |[dbShow] -> 
+                                    cache.CacheShow showName dbShow
+                                    return Some dbShow
+                                |_ -> return None
+            |None -> return None
+        }
